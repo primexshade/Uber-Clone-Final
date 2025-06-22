044f90ec7159270009645bf95075c8b7ecd57aba
Aryan Tiwari <taryan54@gmail.com>
2025-06-23 01:24:54 +0530

    latest commit

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -1,4 +1,5 @@
 const userModel = require('../models/user.model');
+const captainModel = require('../models/captain.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const blackListTokenModel = require('./blackListToken.model');
@@ -22,5 +23,27 @@ module.exports.authUser = async (req , res , next) => {
             return next();
     }catch (error) {
         return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
+
+module.exports.authCaptain = async (req , res , next) => {
+    const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access denied. No token provided.' });
+    }
+
+    const isBlacklisted = await blackListTokenModel.findOne({ token });
+    if (isBlacklisted) {
+        return res.status(401).json({ message: 'Token is blacklisted. Please login again.' });
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const captain = await captainModel.findById(decoded._id)
+        req.captain = captain;
+        return next(); // <-- ADD THIS LINE
+    }catch (err){
+        res.status(401).json({ message: 'Unauthorized' });
     }
 }
